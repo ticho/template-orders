3f0fd7fcb01a6f36d564cd9bc10a8d83aef5d1fa
ticho <tibofolly@gmail.com>
2018-09-01 16:15:05 +0200

    installed css, at this point the site already looks good

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,11 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( 
+  inspinia/animate.css
+  inspinia/bootstrap.min.css
+  inspinia/datepicker3.css
+  inspinia/font-awesome.css
+  inspinia/footable.core.css
+  inspinia/style.css
+)
