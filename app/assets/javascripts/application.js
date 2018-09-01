158ca9891118d93bde1007fd5d266d23c30e27fd
ticho <tibofolly@gmail.com>
2018-09-01 16:28:31 +0200

    Added js. added jquery file to application.js. No more console errors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require inspinia/jquery-3.1.1.min.js
 //= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -11,11 +13,19 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( 
+Rails.application.config.assets.precompile += %w(
   inspinia/animate.css
   inspinia/bootstrap.min.css
   inspinia/datepicker3.css
   inspinia/font-awesome.css
   inspinia/footable.core.css
   inspinia/style.css
+  inspinia/jquery-3.1.1.min.js
+  inspinia/bootstrap.min.js
+  inspinia/bootstrap-datepicker.js
+  inspinia/footable.all.min.js
+  inspinia/inspinia.js
+  inspinia/jquery.metisMenu.js
+  inspinia/jquery.slimscroll.min.js
+  inspinia/pace.min.js
 )
